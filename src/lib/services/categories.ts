261a9cd8639ba1abbfda52ad6648736824256de4
Abdallah Ayman <abdallahayman834@gmail.com>
2025-09-09 16:30:27 +0300

    second commit

--- src/lib/services/categories.ts
+++ src/lib/services/categories.ts
@@ -1,5 +1,7 @@
 export async function getAllCategories() {
-  const response = await fetch(`${process.env.API_BASE_URL}/api/v1/categories`);
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/v1/categories`
+  );
   const allCategories = await response.json();
   if (!response.ok) {
     return { error: response.statusText };
